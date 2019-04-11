c03eb1a4aa3c201a634e8ec0c407095af47894a9
satrancci <thescarface1993@gmail.com>
2019-04-11 13:01:27 -0500

    Updated the script to subset only users that registered in 2018

--- shell-scripts/yearSubsetter3.sh
+++ shell-scripts/yearSubsetter3.sh
@@ -1,2 +1,2 @@
-cat ${1}.txt | tail -n +2 | cut -f 1,7 | egrep '(/17 |/18 |/19 )' | tr "\t" "," | sed 's/,.*$//' > ${1}_171819.txt
-cat ${1}.txt | egrep -f ${1}_171819.txt > ${1}_171819_subs.txt
+cat ${1}.txt | tail -n +2 | cut -f 1,7 | egrep '/18 ' | tr "\t" "," | sed 's/,.*$//' > ${1}_18.csv
+cat ${1}.txt | egrep -f ${1}_18.csv > ${1}_18_subs.txt
