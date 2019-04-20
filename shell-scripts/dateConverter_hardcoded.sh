44538725de4dd8e4b3818204b6f4f496eb182844
satrancci <thescarface1993@gmail.com>
2019-04-19 19:59:51 -0500

    calculates the difference between two dates using datediff from dateutils package

--- shell-scripts/dateConverter_hardcoded.sh
+++ shell-scripts/dateConverter_hardcoded.sh
@@ -1,1 +1,2 @@
-cat times.txt | sed 's/ .*//' | sed 's/ *$//' | sed 's/\//-/g' | sed 's/\([^-]*\)-\([^-]*\)-\([^-]*\)$/20\3-\1-\2/'
+cat ${1}.txt | sed 's/ .*//' | sed 's/ *$//' | sed 's/\//-/g' | sed 's/\([^-]*\)-\([^-]*\)-\([^-]*\)$/20\3-\1-\2/' > ${1}_2.txt
+cat ${1}_2.txt | xargs datediff 2019-4-3 | sed 's/-//' > ${1}_converted.txt
