6f549c0af843f49e3e1b7b7b12b8650baefbe1ec
satrancci <thescarface1993@gmail.com>
2019-04-19 20:06:08 -0500

    removing the old script

--- shell-scripts/dateConverter_hardcoded.sh
+++
@@ -1,2 +0,0 @@
-cat ${1}.txt | sed 's/ .*//' | sed 's/ *$//' | sed 's/\//-/g' | sed 's/\([^-]*\)-\([^-]*\)-\([^-]*\)$/20\3-\1-\2/' > ${1}_2.txt
-cat ${1}_2.txt | xargs datediff 2019-4-3 | sed 's/-//' > ${1}_converted.txt
